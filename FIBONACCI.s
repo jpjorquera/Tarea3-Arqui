ee4b06f8cf1186f773bffa1137d5e58c6ee95189
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2017-06-02 19:21:01 -0400

    ordenado input, agregados comentarios

--- FIBONACCI.s
+++ FIBONACCI.s
@@ -1,18 +1,22 @@
 .data
+# INPUT
 ENESIMO: .word 46
+# PRINTS
+RESULTADO: .asciiz "El término enésimo de la sucesión de Fibonacci es: "
 
 .text
+# MAIN
 main:
-	lw $a0, ENESIMO			# Muere en el 47
-	add $s0, $zero, $zero
+	lw $a0, ENESIMO				# Guardar enesimo para comparar
+	add $s0, $zero, $zero		# Alacenar primeros valores en s0 y s1
 	addi $s1, $zero, 1
-	beq $s0, $a0, PRIMERO
+	beq $s0, $a0, PRIMERO 		# Casos bases
 	beq $s1, $a0, SEGUNDO
-	addi $t0, $zero, 1
+	addi $t0, $zero, 1			# Iterador
 	j LOOP
 
 PRIMERO:
-	add $a0, $s0, $zero
+	add $a0, $s0, $zero			# Almacenar el termino correspondiente
 	j EXIT
 
 SEGUNDO:
@@ -20,15 +24,19 @@ SEGUNDO:
 	j EXIT
 
 LOOP:
-	addi $t0, $t0, 1
-	add $t1, $s0, $s1
-	add $s0, $s1, $zero
+	addi $t0, $t0, 1			# Avanzar iterador
+	add $t1, $s0, $s1			# Calcular siguiente
+	add $s0, $s1, $zero			# Actualizar valores
 	add $s1, $t1, $zero
-	bne $t0, $a0, LOOP
-	add $a0, $s1, $zero
-	j EXIT
+	bne $t0, $a0, LOOP			# Si no ha terminado de iterar
+	add $a0, $s1, $zero			# Si termino: guardar actual
 
 EXIT:
+	add $t0, $a0, $zero			# Auxiliar
+	la $a0, RESULTADO			# Imprimir string
+	li $v0, 4
+	syscall
+	add $a0, $t0, $zero			# Imprimir resultado
 	li $v0, 1
 	syscall
 	li $v0, 10
