a85ef529c8ed2631db2691912aa4cc0ea46136b7
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2017-06-11 01:12:49 -0400

    arreglado mcd como funcion

--- MCD.s
+++ MCD.s
@@ -15,24 +15,10 @@ salto: .asciiz "\n"
 main:
 	lw $a0, num1			# Guardar valores
 	lw $a1, num2
-	slt $t0, $a0, $a1		# Verificar menor
-	bne $t0, $zero, mcd 	# Saltar a mcd
-
-swap:						# Dejar menor en a0
-	add $t0, $a0, $zero
-	add $a0, $a1, $zero
-	add $a1, $t0, $zero
-
-mcd:
-	div $a1, $a0			# Dividir por el menor
-	mfhi $s0				# Guardar resto
-	beq $s0, $zero, end		# Verificar comun divisor
-	add $a1, $a0, $zero		# Actualizar valores siguientes
-	add $a0, $s0, $zero
-	j mcd
-
-end:
-	add $t0, $a0, $zero			# Auxiliar
+	jal mcd
+	
+	# Imprimir valor
+	add $t0, $v0, $zero			# Aux
 	la $a0, str					# Imprimir string
 	li $v0, 4
 	syscall
@@ -40,10 +26,33 @@ end:
 	sw $a0, resultado			# Guardar resultado
 	li $v0, 1
 	syscall
-	la $a0, salto
+	la $a0, salto				# Imprimir salto
 	li $v0, 4
 	syscall
-	li $v0, 10
+	li $v0, 10					# Salir
 	syscall
 
 
+
+mcd:
+	add $s0, $a0, $zero			# Guardar valores
+	add $s1, $a1, $zero
+	slt $t0, $s0, $s1			# Verificar menor
+	bne $t0, $zero, calculo 	# Saltar a mcd
+
+swap:						# Dejar menor en s0
+	add $t0, $s0, $zero
+	add $s0, $s1, $zero
+	add $s1, $t0, $zero
+
+calculo:
+	div $s1, $s0			# Dividir por el menor
+	mfhi $t0				# Guardar resto
+	beq $t0, $zero, end		# Verificar comun divisor
+	add $s1, $s0, $zero		# Actualizar valores siguientes
+	add $s0, $t0, $zero
+	j calculo
+
+end:						# Retornar
+	add $v0, $s0, $zero
+	jr $ra
