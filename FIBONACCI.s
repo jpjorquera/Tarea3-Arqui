a38245d639ff74c9b6abb1346be779ca93697a8c
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2017-06-11 00:52:39 -0400

    arreglado uso de fibonacci como funcion

--- FIBONACCI.s
+++ FIBONACCI.s
@@ -13,31 +13,9 @@ SALTO: .asciiz "\n"
 # MAIN
 main:
 	lw $a0, ENESIMO				# Guardar enesimo para comparar
-	add $s0, $zero, $zero		# Alacenar primeros valores en s0 y s1
-	addi $s1, $zero, 1
-	beq $s0, $a0, PRIMERO 		# Casos bases
-	beq $s1, $a0, SEGUNDO
-	addi $t0, $zero, 1			# Iterador
-	j LOOP
-
-PRIMERO:
-	add $a0, $s0, $zero			# Almacenar el termino correspondiente
-	j EXIT
-
-SEGUNDO:
-	add $a0, $s1, $zero
-	j EXIT
-
-LOOP:
-	addi $t0, $t0, 1			# Avanzar iterador
-	add $t1, $s0, $s1			# Calcular siguiente
-	add $s0, $s1, $zero			# Actualizar valores
-	add $s1, $t1, $zero
-	bne $t0, $a0, LOOP			# Si no ha terminado de iterar
-	add $a0, $s1, $zero			# Si termino: guardar actual
-
-EXIT:
-	add $t0, $a0, $zero			# Auxiliar
+	jal FIBONACCI 				# LLamar funcion
+	# Imprimir valor
+	add $t0, $v0, $zero			# Auxiliar
 	la $a0, STR					# Imprimir string
 	li $v0, 4
 	syscall
@@ -50,3 +28,31 @@ EXIT:
 	syscall
 	li $v0, 10
 	syscall
+
+# Funcion principal
+FIBONACCI:
+	add $s0, $zero, $zero		# Alacenar primeros valores en s0 y s1
+	addi $s1, $zero, 1
+	beq $s0, $a0, PRIMERO 		# Casos bases
+	beq $s1, $a0, SEGUNDO
+	addi $t0, $zero, 1			# Iterador
+	j LOOP
+
+PRIMERO:
+	add $v0, $s0, $zero			# Almacenar el termino correspondiente
+	j EXIT
+
+SEGUNDO:
+	add $v0, $s1, $zero
+	j EXIT
+
+LOOP:
+	addi $t0, $t0, 1			# Avanzar iterador
+	add $t1, $s0, $s1			# Calcular siguiente
+	add $s0, $s1, $zero			# Actualizar valores
+	add $s1, $t1, $zero
+	bne $t0, $a0, LOOP			# Si no ha terminado de iterar
+	add $v0, $s1, $zero			# Si termino: guardar actual
+
+EXIT:
+	jr $ra
